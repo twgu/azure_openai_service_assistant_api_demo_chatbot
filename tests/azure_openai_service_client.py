a71862c04fd0f474d663071769aec08ba49a52ba
구태완 <gutw@donga.co.kr>
2025-03-17 11:27:47 +0900

    환경변수명 변경

--- tests/azure_openai_service_client.py
+++ tests/azure_openai_service_client.py
@@ -17,8 +17,8 @@ class AzureOpenAIServiceClient:
         load_dotenv()
 
         self.client = AzureOpenAI(
-            azure_endpoint=os.getenv('ENDPOINT'),
-            api_key=os.getenv('API_KEY'),
+            azure_endpoint=os.getenv('AZURE_OPENAI_ENDPOINT'),
+            api_key=os.getenv('AZURE_OPENAI_API_KEY'),
             api_version=os.getenv('API_VERSION')
         )
 
--- tests/print_load_dotenv.py
+++ tests/print_load_dotenv.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print('\n[ 환경변수 목록 ]')
-print(f'ENDPOINT={os.getenv("ENDPOINT")}')
-print(f'API_KEY={os.getenv("API_KEY")}')
+print(f'AZURE_OPENAI_ENDPOINT={os.getenv("AZURE_OPENAI_ENDPOINT")}')
+print(f'AZURE_OPENAI_API_KEY={os.getenv("AZURE_OPENAI_API_KEY")}')
 print(f'API_VERSION={os.getenv("API_VERSION")}')
-print(f'DEFAULT_ASSISTANT_ID={os.getenv("DEFAULT_ASSISTANT_ID")}')
